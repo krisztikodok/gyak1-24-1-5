acac4eb70f9ef7026cea28072eedae469e9a380e
krisztikodok <135458893+krisztikodok@users.noreply.github.com>
2024-01-05 13:37:41 +0000

    modified

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #try - exceptions (ZeroDivisionError, NameError, ValueError, TypeError)
-
+"""
 try:
   a=100
   b=0
   c=a/b
   print("{}/{}={}".format(a,b,c))
-except ZeroDivisionError:
+except ZeroDivisionError:  #----------
   print("Not dividable")
   print("{}/{}=not possible".format(a,b))
 
@@ -16,12 +16,36 @@ def num_in():
     z=x/y
     print(z)
     if(x<0):
-      raise TypeError
-  except ZeroDivisionError:
+      raise TypeError  #----------
+  except ZeroDivisionError:  #----------
     print("Not dividable by zero")
-  except ValueError:
+  except ValueError:  #----------
     print("Not a number")
-  except TypeError:
+  except TypeError:  #----------
     print("Data is out of range")
 
 num_in()
+"""
+def ve1():
+  try:
+    val=int("abc")
+    print(i)
+  except ValueError as v:
+    print(f"Caught a ValueError at {v}")
+
+ve1()
+
+def ve2():
+  try:
+    val=float("abc")
+    print(i)
+  except ValueError as v:
+    print(f"Caught a ValueError at {v}")
+  else:
+    print("No exception occured")
+
+ve2()
+
+
+def typeErrorCase():
+  
